4e00c72369d2beaeea98493fa4885f8332395b1d
EmiArzSal <arzate.salazar.emiliano@gmail.com>
2024-12-12 01:32:02 -0600

    Cambios para correr en paralelo transpite y electron con solo npm run dev

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,9 +1,15 @@
 import { app, BrowserWindow } from "electron";
 import path from "path";
+import { isDev } from "./util.js";
 
 type test = string;
 
 app.on("ready", () => {
   const mainWindow = new BrowserWindow({width: 800, height: 600});
-  mainWindow.loadFile(path.join(app.getAppPath() + "/dist-react/index.html"));
+  if(isDev()){
+    mainWindow.loadURL("http://localhost:5123");
+  }
+  else{
+    mainWindow.loadFile(path.join(app.getAppPath() + "/dist-react/index.html"));
+  }
 });
